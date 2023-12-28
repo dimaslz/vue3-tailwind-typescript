41ad04c673ea0ba5ac34d353cd9314cd069bd36b
faeze-73 <faeze.fallahnezhad73@gmail.com>
2023-12-28 22:47:44 +0330

    add initial file

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,15 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { loadGlobalComponents } from "../globalComponents";
 
 import "./assets/css/tailwind.css";
 
-createApp(App).use(store).use(router).mount("#app");
+// First, create the app instance
+const app = createApp(App);
+
+// Load global components
+loadGlobalComponents(app);
+
+// Install plugins
+app.use(store).use(router).mount("#app");
